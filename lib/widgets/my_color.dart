9c2edf22eabf5796d26defc6f9ad9ea9fd6fea00
nafisah <nafisahisbrsani12@gmail.com>
2024-09-24 07:35:38 +0700

    add color

--- lib/widgets/my_color.dart
+++ lib/widgets/my_color.dart
@@ -6,5 +6,7 @@ const Color textFieldColor = Color(0xFFD9D9D9);
 const Color firstColor = Color(0xFFFFFFFF);
 const Color chairColor = Color(0xFF5E351F);
 const Color facebookColor = Color(0xFF448AFF);
+const Color handwashcolor = Color(0xFF4C4C4F);
+
 
 
